0ed4da301ec8ffc74a5a504626316e3d9cea325e
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2025-01-19 16:43:01 +0100

    group anagrams

---
+++ Array&Hashing/group_anagrams.py
@@ -0,0 +1,21 @@
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        
+        for st in strs:
+            chars_count = [0] * 26
+            for c in st:
+                chars_count[ord(c) - ord('a')] += 1
+            
+            anagrams[tuple(chars_count)].append(st)
+            
+        return [word_list for key, word_list in anagrams.items()]
+    
+sol = Solution()
+in1 = ["act","pots","tops","cat","stop","hat"]
+
+print(sol.groupAnagrams(in1))
