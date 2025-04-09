53c631cba3aad977cda2af1b553ed2416b643dab
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2025-04-09 18:59:42 +0200

    add solution for printing a binary tree in a structured format

--- trees/tree_utils.py
+++ trees/tree_utils.py
@@ -1,5 +1,5 @@
 
-from typing import Optional
+from typing import Optional, List
 from collections import deque
 
 class TreeNode:
@@ -24,3 +24,32 @@ def print_tree(root: Optional[TreeNode]):
                 to_visit.append(node.right)
         
         print(visited)
+        
+class Solution:
+            
+    def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
+        
+        def get_height(node: Optional[TreeNode]) -> int:
+            if node is None:
+                return 0
+            
+            return 1 + max(get_height(node.left), get_height(node.right))
+        
+        def rec_print_tree(node: Optional[TreeNode], l, r, level):
+            if node is None:
+                return
+            
+            mid = l + (r - l) // 2
+            
+            res[level][mid] = str(node.val)
+            
+            rec_print_tree(node.left, l, mid, level + 1)
+            rec_print_tree(node.right, mid, r, level + 1)
+            
+        height = get_height(root)
+        ncols = 2**(height) - 1
+        res = [[""] * ncols for _ in range(height)]
+        
+        rec_print_tree(root, 0, ncols, 0)
+        
+        return res
