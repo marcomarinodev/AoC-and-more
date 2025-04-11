cd4b8120c5284ee6c069a488418be8900d2a0d42
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2025-04-11 09:56:55 +0200

    add solution for finding repeated DNA sequences

---
+++ SlidingWindow/repeated_dna_sequences.py
@@ -0,0 +1,16 @@
+
+from typing import List
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        ans, seen = set(), set()
+        for i in range(len(s)-9):
+            dna = s[i:i+10]
+            if dna in seen:
+                ans.add(dna)
+            else:
+                seen.add(dna)
+        return list(ans)
+    
+sol = Solution()
+print(sol.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"))
